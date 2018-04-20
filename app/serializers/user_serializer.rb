3bfd4d99a0b3943ea0daa4c42a4df89913e72b0d
Rameshwor Dhakal <dhakalramu2070@gmail.com>
2018-04-20 18:29:03 +0545

    signup null value fixes, event addition

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,3 @@
+class UserSerializer < ActiveModel::Serializer 
+    attributes :name, :email, :password, :name, :role, :address 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409142715) do
+ActiveRecord::Schema.define(version: 20180420083137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "desc"
+    t.datetime "date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
