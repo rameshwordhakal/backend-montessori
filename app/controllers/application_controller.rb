2c498be682f922f55f3360ace3efeb9ea76930f3
Rameshwor Dhakal <dhakalramu2070@gmail.com>
2018-04-24 13:01:03 +0545

    cors issue fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :created_at, :updated_at, :email, :role, :contact, :gender, :address])    
   end
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+  
+
 end
